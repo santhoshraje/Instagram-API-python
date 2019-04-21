fa32ef19710ddeb018082e3b1a15b12de8ff5fd7
Santhoshraj <38834149+santhoshraje@users.noreply.github.com>
2019-04-21 12:39:58 +0800

    Update upload_photo.py
     - photos are deleted after uploading to remove double posts
     - check for corrupted files
     - if corrupted file is detected, go to next file and upload that instead
     - support for custom caption for each photo

--- upload_photo.py
+++ upload_photo.py
@@ -2,12 +2,32 @@
 # -*- coding: utf-8 -*-
 #
 # Use text editor to edit the script and type in valid Instagram username/password
-
 from InstagramAPI import InstagramAPI
+from random import randint
+import os
 
-InstagramAPI = InstagramAPI("login", "password")
-InstagramAPI.login()  # login
+username = ""
+password = ""
+photocaption = "Comment Amen if you love God!\n.\n#jesus #love #christian #christianity #amen #christianblogger #christianlife #christianmom #christiangirl #christianliving #christianwoman #faith #grace #truth #choosejoy #Godslove #GodlovesYou #BibleVerse"
+InstagramAPI = InstagramAPI(username, password)
+InstagramAPI.login()
 
-photo_path = '/path/to/photo.jpg'
-caption = "Sample photo"
-InstagramAPI.uploadPhoto(photo_path, caption=caption)
+q = 1
+while q:
+ i = randint(0, 1000)
+ photo_path = '/home/user/Pictures/' + str(i) + '.jpg'
+
+ if os.path.exists(photo_path):
+  caption = photocaption
+  try:
+    InstagramAPI.uploadPhoto(photo_path, caption=caption)
+  except RuntimeError:
+    print("Corrupted file detected. File has been deleted.")
+    os.remove(photo_path)
+    continue
+  q = 0
+  print(photo_path + " has been successfully uploaded.")
+  os.remove(photo_path)
+ else:
+  continue
+
